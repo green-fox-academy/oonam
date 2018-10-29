9d50d017e05fafe77a4165e9eca4337862559bb7
Akos Makovics <akos.makovics@gmail.com>
2018-10-29 12:48:48 +0100

    w02d1 am ex1-3

---
+++ week-02/day-1/src/com/company/HelloOthers.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Andor!");
+        System.out.println("Hello, Dodo!");
+        System.out.println("Hello, Andi!");
+    }
+}
