06f284f17bb8584dacb1dc4dc36a2acc9f15428c
Akos Makovics <akos.makovics@gmail.com>
2018-10-29 19:07:35 +0100

    Cuboid remake

--- week-02/day-1/src/com/company/Cuboid.java
+++ week-02/day-1/src/com/company/Cuboid.java
@@ -2,11 +2,14 @@ package com.company;
 
 public class Cuboid {
     public static void main(String[] args) {
-        double area = 600;
-        double volume = 1000;
+        double l = 10;
+        double w = 10;
+        double h = 10;
 
+        double area = l * w;
+        double volume = l * w * h;
 
-
-
+        System.out.println("Surface area: " + area);
+        System.out.println("Volume: " + volume);
     }
 }
