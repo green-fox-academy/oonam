f41618c1b5b1139dd16f966270eb068e94e90d0a
Akos Makovics <akos.makovics@gmail.com>
2018-11-06 14:35:10 +0100

    Reverse

---
+++ week-03/day-2/src/com/gfaw03d2/Reverse.java
@@ -0,0 +1,8 @@
+package com.gfaw03d2;
+
+public class Reverse {
+    public static void main(String[] args) {
+        StringBuilder rev = new StringBuilder(".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI").reverse();
+        System.out.println(rev);
+    }
+}
