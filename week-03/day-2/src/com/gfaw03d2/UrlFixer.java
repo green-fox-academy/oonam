709d5bac11aaac0ed3f606013c528b0eff27580d
Akos Makovics <akos.makovics@gmail.com>
2018-11-06 13:24:22 +0100

    UrlFixer

---
+++ week-03/day-2/src/com/gfaw03d2/UrlFixer.java
@@ -0,0 +1,12 @@
+package com.gfaw03d2;
+
+public class UrlFixer {
+    public static void main(String... args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+        String url2 = url.replace("bots", "odds");
+        
+        System.out.println(url2);
+
+
+    }
+}
