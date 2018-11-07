b14b1ea4052027e61eb2f1c8b5a76417d59ab6a0
Akos Makovics <akos.makovics@gmail.com>
2018-11-07 15:43:03 +0100

    Checkerboard

---
+++ week-03/day-3/src/com/gfaw03d3/Checkerboard.java
@@ -0,0 +1,75 @@
+package com.gfaw03d3;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+
+//        int x;
+//        int y;
+//        int width = 40;
+//
+//        for (int i = 0; i < 8; i++) {
+//
+//            for (int j = 0; j < 8; j++) {
+//                x = j * width;
+//                y = i * width;
+//                if ((i % 2) == (j % 2))
+//                    graphics.setColor(Color.WHITE);
+//                else
+//                    graphics.setColor(Color.BLACK);
+//                graphics.fillRect(x, y, 40, 40);
+//            }
+//        }
+
+
+        int x = 10;
+        int y = 10;
+        int size = 20;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 == 0 ^ j % 2 == 0) {
+                    graphics.setColor(Color.WHITE);
+                } else {
+                    graphics.setColor(Color.BLACK);
+                }
+
+                graphics.fillRect(x, y, size, size);
+                x += size;
+            }
+            x = 10;
+            y += size;
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
