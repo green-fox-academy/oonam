fc46c2c6298912c44a2e8168cbd2c1a2824b33b4
Akos Makovics <akos.makovics@gmail.com>
2018-11-08 17:27:50 +0100

    JosephusProblem

---
+++ week-03/day-4/src/com/gfaw03d4/JosephusProblem.java
@@ -0,0 +1,17 @@
+package com.gfaw03d4;
+
+public class JosephusProblem {
+    public static void main(String[] args) {
+        int n = 12;
+        int k = 2;
+        System.out.println("The chosen place is " + josephus(n, k) + ".");
+    }
+
+    public static int josephus(int n, int k) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return (josephus(n - 1, k) + (k-1)) % n + 1;
+        }
+    }
+}
