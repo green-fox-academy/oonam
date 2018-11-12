42da9e965b8a4c75254215a1ce29083c946d3e20
Akos Makovics <akos.makovics@gmail.com>
2018-11-12 12:39:52 +0100

    PrintEachLine

---
+++ week-04/day-1/src/com/gfaw04d1/PrintEachLine.java
@@ -0,0 +1,24 @@
+package com.gfaw04d1;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+public class PrintEachLine {
+	public static void main(String[] args) {
+		// Write a program that opens a file called "my-file.txt", then prints
+		// each of the lines form the file.
+		// If the program is unable to read the file (for example it does not exists),
+		// then it should print an error message like: "Unable to read file: my-file.txt"
+
+		try {
+			Path filePath = Paths.get("myfile.txt");
+			List<String> lines = Files.readAllLines(filePath);
+			System.out.println(lines.get(0));
+		} catch (Exception e) {
+			System.out.println("Uh-oh, could not read the file!");
+		}
+
+	}
+}
