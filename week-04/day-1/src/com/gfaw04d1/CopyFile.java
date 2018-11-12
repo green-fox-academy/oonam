e7081f1a986400a65d062d694e4b1c4a66c45717
Akos Makovics <akos.makovics@gmail.com>
2018-11-12 16:40:28 +0100

    another CopyFile

--- week-04/day-1/src/com/gfaw04d1/CopyFile.java
+++ week-04/day-1/src/com/gfaw04d1/CopyFile.java
@@ -9,34 +9,22 @@ import java.util.List;
 public class CopyFile {
 	public static void main(String[] args) {
 		String filenameIn = "my-file2.txt";
-		String filenameOut = "my-fileOut.txt";
+		String filenameOut = "my-fileOut2.txt";
 
-		copyAFile(filenameIn, filenameOut);
+		System.out.println(copyAFile(filenameIn, filenameOut));
 	}
 
-	public static void copyAFile(String filenameIn, String filenameOut) {
+	public static boolean copyAFile(String filenameIn, String filenameOut) {
 		List<String> lines = new ArrayList();
 
 		try {
 			Path fNameIn = Paths.get(filenameIn);
-			lines = Files.readAllLines(fNameIn);
-			System.out.println("Input: " + lines);
-		} catch (Exception e) {
-			System.out.println("Unaccessible path");
-		}
-
-		try {
 			Path fNameOut = Paths.get(filenameOut);
+			lines = Files.readAllLines(fNameIn);
 			Files.write(fNameOut, lines);
-//			if (lines == Files.readAllLines(fNameOut)) {
-//				System.out.println(true);
-//				System.out.print("Output: " + Files.readAllLines(fNameOut));
-//			} else {
-//				System.out.println(false);
-//			}
-			System.out.print("Output: " + Files.readAllLines(fNameOut));
+			return true;
 		} catch (Exception e) {
-			System.out.println("Unable to write file: my-file.txt");
+			return false;
 		}
 	}
 }
