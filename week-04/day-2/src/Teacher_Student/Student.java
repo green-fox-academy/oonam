545c853f9d559185f8434fea94a7f64e11d54685
Akos Makovics <akos.makovics@gmail.com>
2018-11-13 15:50:18 +0100

    Teacher Student

---
+++ week-04/day-2/src/Teacher_Student/Student.java
@@ -0,0 +1,10 @@
+package Teacher_Student;
+
+public class Student {
+	public void question(Teacher teacher) {
+		teacher.answer();
+	}
+
+	public void learn() {
+	}
+}
---
+++ week-04/day-2/src/Teacher_Student/Teacher.java
@@ -0,0 +1,10 @@
+package Teacher_Student;
+
+public class Teacher {
+	public void teach(Student student) {
+		student.learn();
+	}
+
+	public void	answer() {
+	}
+}
