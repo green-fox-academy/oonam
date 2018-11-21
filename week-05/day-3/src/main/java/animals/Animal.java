f79a5793ec6814d858a492bd9a5638e6d340b35e
Akos Makovics <akos.makovics@gmail.com>
2018-11-21 21:11:39 +0100

    Zoo

---
+++ week-05/day-3/src/main/java/animals/Animal.java
@@ -0,0 +1,22 @@
+package main.java.animals;
+
+abstract public class Animal {
+  protected String name;
+  protected int age;
+  protected String gender;
+  protected String color;
+
+  public Animal(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  abstract public String breed();
+
+  public void walk(){
+
+  }
+}
