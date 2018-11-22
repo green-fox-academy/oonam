31e7f38a3c4207f240d1656e355b669138d08a46
Akos Makovics <akos.makovics@gmail.com>
2018-11-22 12:44:06 +0100

    Apples

--- week-05/day-4/Apples/test/ApplesTest.java
+++ week-05/day-4/Apples/test/ApplesTest.java
@@ -3,8 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ApplesTest {
+
   @Test
   public void getApple() {
+
     Apples apple = new Apples();
     String message = "apple";
 
--- week-05/day-4/src/Apples.java
+++
@@ -1,5 +0,0 @@
-public class Apples {
-    public String getApple() {
-        return "apple";
-    }
-}
